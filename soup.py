d5d51a85747b03805fc1fb321338b9956d05b331
Rahul <rahulprabhakar0596@gmail.com>
2018-04-04 20:27:26 +0530

    Small Fixes

--- soup.py
+++ soup.py
@@ -3,36 +3,6 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def searchBingMeaning(query):
-    s = requests.Session()
-    s.headers[
-        'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-    r = s.get("https://www.bing.com/search?q=meaning+of+{}".format(query))
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-    print(soup.find('div', {'class': "dc_mn"}).text)
-
-
-def searchBingWeather(location):
-    s = requests.Session()
-    s.headers[
-        'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-    r = s.get("https://www.bing.com/search?q=weather+in+{}".format(location))
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-    print(soup.find('div', {'class': "wtr_currTemp b_focusTextLarge"}).text)
-
-
-def translateBing(word, language):
-    s = requests.Session()
-    s.headers[
-        'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-    r = s.get("https://www.bing.com/search?q=translate+{}+in+{}".format(word, language))
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-    print(soup.find('textarea', attrs={'id': "tta_tv"}).text)
-
-
 def searchWeatherDetails(location):
     s = requests.Session()
     s.headers[
@@ -49,20 +19,18 @@ def searchWeatherDetails(location):
 
 def searchWordMeaning(query):
     s = requests.Session()
-    s.headers[
-        'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
     r = s.get("https://www.google.co.in/search?ie=UTF-8&q=meaning+of+{}".format(query))
     data = r.text
     soup = BeautifulSoup(data, "html.parser")
-    var = soup.find('div', attrs={'class': 'PNlCoe'}).text
-    result = "The meaning for {} is {}".format(query, var)
+    var = soup.find('div', attrs={'class': 'PNlCoe'}).text.split('.')[0]
+    result = "{} means {}".format(query, var)
     return result
 
 
 def translateGoogle(word, language):
     s = requests.Session()
-    s.headers[
-        'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
     r = s.get('https://www.google.co.in/search?q=translate+{}+in+{}&ie=UTF-8'.format(word, language))
     data = r.text
     soup = BeautifulSoup(data, "html.parser")
@@ -71,4 +39,4 @@ def translateGoogle(word, language):
 
 
 if __name__ == '__main__':
-    print(translateGoogle("auspicious", "tamil"))
+    print(searchWordMeaning("life"))
