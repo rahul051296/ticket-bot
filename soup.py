4b21abbf2e109fd1d125b37f3f74e0d06b0b5020
rahul051296 <rahulprabhakar0596@gmail.com>
2018-04-04 10:53:37 +0530

    Added Soup

--- soup.py
+++ soup.py
@@ -4,25 +4,58 @@ import requests
 
 
 def searchBingMeaning(query):
-    r = requests.get("https://www.bing.com/search?q=meaning+of+{}".format(query))
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    r = s.get("https://www.bing.com/search?q=meaning+of+{}".format(query))
     data = r.text
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, "html.parser")
     print(soup.find('div', {'class': "dc_mn"}).text)
 
 
 def searchBingWeather(location):
-    r = requests.get("https://www.bing.com/search?q=weather+in+{}".format(location))
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    r = s.get("https://www.bing.com/search?q=weather+in+{}".format(location))
     data = r.text
-    soup = BeautifulSoup(data)
-    print(soup.find('div', { 'class': "wtr_currTemp b_focusTextLarge"}).text)
+    soup = BeautifulSoup(data, "html.parser")
+    print(soup.find('div', {'class': "wtr_currTemp b_focusTextLarge"}).text)
 
 
 def translateBing(word, language):
-    r = requests.get("https://www.bing.com/search?q=translate+{}+in+{}".format(word,language))
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    r = s.get("https://www.bing.com/search?q=translate+{}+in+{}".format(word, language))
     data = r.text
-    soup = BeautifulSoup(data)
-    print(soup.find('textarea', {'id': "tta_tv"}).text)
+    soup = BeautifulSoup(data, "html.parser")
+    print(soup.find('textarea', attrs={'id': "tta_tv"}).text)
+
+
+def searchGoogleWeather(location):
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    r = s.get("https://www.google.co.in/search?q=weather+in+{}&oq=weather+inche&aqs=chrome.1.69i57j0l5.5039j0j7&sourceid=chrome&ie=UTF-8".format(location))
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+    print(soup.find('span', attrs={'class': 'wob_t'}).text)
+
+
+def searchGoogleMeaning(query):
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    r = s.get("https://www.google.co.in/search?ie=UTF-8&q=meaning+of+{}".format(query))
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+    print(soup.find('div', attrs={'class': 'PNlCoe'}).text)
+
+
+def translateGoogle(word, language):
+    s = requests.Session()
+    s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+    r = s.get('https://www.google.co.in/search?q=translate+{}+in+{}&ie=UTF-8'.format(word, language))
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+    print(soup.find('pre', attrs={'id': 'tw-target-text'}).text)
 
 
 if __name__ == '__main__':
-    translateBing("wheel", "tamil")
+    searchGoogleWeather('chennai')
